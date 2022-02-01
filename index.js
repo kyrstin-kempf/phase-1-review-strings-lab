a723c41b77ddb08f8dad9f551e216c2fad5e60a6
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-01-31 21:19:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+
+const currentUser = 'Carrie';
+
+// const welcomeMessage = 'Welcome to Flatbook, '; -- need to add currentUser
+// const welcomeMessage = currentUser; -- need welcomeMessage
+
+// INTERPOLATION -- 
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// CONCATENATION --
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser+'!';
+
+// avoding repeating ourselves and use .toUpperCase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
